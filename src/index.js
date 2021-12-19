5f514873863759a43859a868eb8b4c4df3e9ddde
jobjoey2021 <84551590+jobjoey2021@users.noreply.github.com>
2021-12-19 15:15:18 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<h2>  同步算成功了 2021/12/19</h2>
+<h2>可以用 iPad 在 bed coding 了</h2>
+<h2>iPad; play.js; codeSandbox win chrome; github; vscode; netlify; apps html;
+</h2>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+
+
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
