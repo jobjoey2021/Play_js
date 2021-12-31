32076ed6a1252af8981b0b38e2dddd7d8b14bc09
jobjoey2021 <84551590+jobjoey2021@users.noreply.github.com>
2021-12-31 12:15:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+<h2>  NB on github sync test 2021/12/31</h2>
 <h2>  同步算成功了 2021/12/19</h2>
 <h2>可以用 iPad 在 bed coding 了</h2>
 <h2>iPad; play.js; codeSandbox win chrome; github; vscode; netlify; apps html;
